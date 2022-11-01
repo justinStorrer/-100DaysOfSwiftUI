7f9da5c02674c37d96f7f167cc38f15e2bb894c0
Justin Storrer <59339909+justinStorrer@users.noreply.github.com>
2022-11-01 08:42:43 -0400

    Add files via upload

---
+++ CustomModifier/CustomModifier/CustomModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomModifierApp.swift
+//  CustomModifier
+//
+//  Created by Justin Storrer on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
